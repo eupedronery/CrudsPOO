e79c1c7c1e94d29352580d09517c77c967d94651
Pedro Nery <100791953+eupedronery@users.noreply.github.com>
2022-12-03 22:23:19 -0300

    Add files via upload

---
+++ PagamentoDAOImplement.java
@@ -0,0 +1,138 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pagamentos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class PagamentoDAOImplement implements PagamentoDAO {
+	
+	//Metodo de conexao com bando de dados
+	
+	public final static String hostName = "";
+	public final static String dbName = "";
+	public final static String user = "";
+	public final static String senha = "";
+	private Connection con; //variavel que permite a conexao com banco de dados
+
+	
+	public PagamentoDAOImplement() {
+
+		try {
+		Class.forName("net.sourceforge.jtds.jdbc.Driver");	
+		con = DriverManager.getConnection(
+				String.format("",
+						 hostName, dbName, user, senha));
+		} catch(ClassNotFoundException | SQLException e) {
+			
+		}
+	}
+
+	
+	//Metodo DAO de inserção conectado ao botao Salvar
+	@Override
+	public void criar(Pagamento ps) {
+		
+		
+		//INSERT INTO que preenche as informacoes da PIZZASALGADA e o ID da PIZZA
+		
+		String sql = "INSERT INTO PIZZA(ID)"
+				+ "VALUES("+ ps.getId() +")"
+				+ ""
+				+ "INSERT INTO PIZZASALGADA "
+				+ "   (ID_PIZZA_SAL, SABOR, BORDA, PREÇO) "
+				+ "VALUES ("+ ps.getId() +", '"+ ps.getformaPagamento() +"', '"+ ps.getvalorTotal() + ")";
+			
+		try {
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.executeUpdate();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "NÃO É PERMITIDO ID REPETIDO", 
+					"ERRO", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+
+	//Metodo DAO que realiza o pesquisar
+	@Override
+	public List<Pagamento> pesquisarPizzaSalgadaSabor(String sabor) {
+		
+		List<Pagamento> lista = new ArrayList<>();
+		
+		//SELECT * FROM QUE que pesquisar com no sabor sem nenhum valor tras lista todos os registros
+		
+		String sql = "";
+		
+		try {
+		    PreparedStatement pstmt = con.prepareStatement(sql);
+		    ResultSet rs = pstmt.executeQuery();
+		   while(rs.next()) {
+			   Pagamento ps = new Pagamento();
+			   
+			   ps.setformaPagamento(rs.getString("FORMA_PAGAMENTO"));
+			   ps.setId(rs.getInt("CODIGO_PAGAMENTO"));
+			   ps.setvalorTotal(rs.getDouble("VALOR_TOTAL"));
+			   lista.add(ps);
+			   
+		   }
+		}catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, "ERRO NO RETORNO", "ERRO", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		return lista;
+	}
+	
+	
+	//Metodo DAO que apagar os registros incluindo a chave primaria
+	public void apagar(Pagamento ps) {
+		
+		//DELETE que apgas as pizzas dos tanto da tabela PIZZA quanto da tabela PIZZASALGADA
+		
+		String sql = " ";
+		
+		String sql2 = " ";
+		
+		
+		try {
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.executeUpdate();	
+			
+		PreparedStatement pstmt2 = con.prepareStatement(sql2);
+		pstmt2.executeUpdate();
+		
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "ERRO NA DELETAÇÃO", "ERRO", JOptionPane.ERROR_MESSAGE);
+		}
+		
+	}
+	
+	//Metodo DAO que permite na edicao dos valores haver atualizar deles menos do id pois é chave primaria
+	public void atualizar(String saborAntigo, Pagamento ps) {
+		
+		
+		//UPDATE que atualizar as informacoes da pizza salgada menos seu ID por ser a chave primaria
+		
+		String sql = "UPDATE PIZZASALGADA "
+				       + "SET ID_PIZZA_SAL = "+ps.getId()+", "
+				       + "SABOR = '"+ps.getformaPagamento()+"', "
+				       + "BORDA = '"+ps.getvalorTotal()+"', "
+				       + "WHERE SABOR = '"+saborAntigo+"'";
+                
+		try {
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.executeUpdate();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "NÃO ALTERAR A CHAVE PRIMARIA", 
+					"ERRO", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+}
